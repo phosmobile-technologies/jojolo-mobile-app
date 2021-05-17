c1c4a85ac1847efe65ea3dd036a3c3dde90f7397
david <a1@Davids-MacBook-Pro.local>
2021-05-17 17:17:37 +0100

    added tags page

---
+++ src/modules/forum/components/tag.component.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { View, ScrollView, TouchableOpacity, StyleSheet } from "react-native";
+import { COLORS } from "../../../constants";
+import AppText from "../../common/components/typography/text.component";
+
+const TagComponent = ({ tag }: { tag: any }) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity>
+        <View style={styles.tag}>
+          <AppText>{tag.item.name}</AppText>
+          <AppText>{tag.item.number}</AppText>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 5,
+    marginHorizontal: 35,
+    marginTop: 10,
+  },
+  tag: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 2,
+    backgroundColor: COLORS.APP_PRIMARY_COLOR,
+    padding: 25,
+    width: 300,
+    margin: 0,
+    borderRadius: 7,
+    paddingVertical: 30,
+  },
+});
+
+export default TagComponent;

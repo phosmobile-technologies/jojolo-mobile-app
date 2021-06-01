7507296e0ed99771fe913214ab93bea9308d867c
david <a1@Davids-MacBook-Pro.local>
2021-06-01 13:52:02 +0100

    The tags should be sorted by the number of posts that are tagged with said tags

--- src/modules/forum/screens/tags.screen.tsx
+++ src/modules/forum/screens/tags.screen.tsx
@@ -12,23 +12,23 @@ const tags = [
   {
     id: "1",
     name: "Test Tag 1",
-    number: "3",
+    number: 3,
   },
   {
     id: "2",
     name: "Test Tag 2",
-    number: "4",
+    number: 4,
   },
   {
     id: "3",
     name: "Test Tag 3",
-    number: "3",
+    number: 11,
   },
-  // {
-  //   id: "4",
-  //   name: "Feeding",
-  //   number: "15",
-  // },
+  {
+    id: "4",
+    name: "Feeding",
+    number: "15",
+  },
   // {
   //   id: "5",
   //   name: "Stooling",
@@ -41,13 +41,27 @@ const tags = [
   // },
 ];
 
+/**
+ * Function For rendering the Tags Page
+ * @returns
+ *
+ */
 export const TagsPage = () => {
+  /**
+   *
+   * Function To Sort Array based on The Highest number
+   */
+  let SortedByTagNumberOfPosts = tags.slice(0);
+  SortedByTagNumberOfPosts.sort((a: any, b: any) => {
+    return b.number - a.number;
+  });
+
   return (
     <ScrollView style={styles.container}>
       <View>
         <FlatList
           style={styles.feed}
-          data={tags}
+          data={SortedByTagNumberOfPosts}
           renderItem={(item: any) => <TagComponent tag={item} />}
           keyExtractor={(item: any) => item.id}
           showsVerticalScrollIndicator={false}
